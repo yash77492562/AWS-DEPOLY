cbae16b109f4a9915fcef62ff10944d3e8c69e3a
yash77492562 <138091586+yash77492562@users.noreply.github.com>
2024-10-29 15:57:31 +0530

    Add files via upload

---
+++ Deploying npm , Monorepos/index.ts
@@ -0,0 +1,21 @@
+// in order to depoly our code in npm 
+// 1. npm  login 
+// 2. in package.json "name":@yash7749/common,
+// 3. "main":"dist/index.js"
+// 4. npm publish --access=public
+
+
+//  in tsconfig.json  :- "declaration":true 
+// in order to make a declaration file .d.ts file 
+
+
+
+
+// Mono repos
+// npm workspaces
+// Turborepo
+// Lerna
+
+// POPULAR ONE  :- Turbo (repo)
+// https://github.com/calcom/cal.com/blob/main/package.js
+// npx create-turbo@latest
